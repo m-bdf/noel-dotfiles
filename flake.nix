bef385025fd6c59e8a5ed0349208ddb3564b4f07
NoelHuibers <noel@huibers.io>
2024-11-06 01:31:38 +0100

    Init NixOS

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let
+	system = "x86_64-linux";
+	pkgs = import nixpkgs {
+	  inherit system;
+	  config = {
+	    allowUnfree = true;
+	  };
+	};
+    in {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem { 
+        specialArgs = { inherit inputs system; };
+        modules = [
+           ./hosts/default/configuration.nix
+	];
+      };
+      workstation = nixpkgs.lib.nixosSystem {
+        extraSpecialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/workstation/configuration.nix
+	];
+      };
+    };
+  }; 
+}
