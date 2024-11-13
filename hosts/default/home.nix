292008dfc3be5d30cfe4992ae9fd231235c019a2
NoelHuibers <noel@huibers.io>
2024-11-13 14:00:41 +0100

    Add git file

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -6,16 +6,10 @@
 
   imports = [
     (import ./../../modules/home-manager/vscode.nix { inherit pkgs; })
+    (import ./../../modules/home-manager/git.nix)
   ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -75,15 +69,6 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  programs.git = {
-    enable = true;
-    userName = "NoelHuibers";
-    userEmail = "noel@huibers.io";
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
---
+++ modules/home-manager/git.nix
@@ -0,0 +1,10 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "NoelHuibers";
+    userEmail = "noel@huibers.io";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+}
