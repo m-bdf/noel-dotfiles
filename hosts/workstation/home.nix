0bbd7538938fb9d1d1d36795c0fe20559d9710dd
NoelHuibers <noel@huibers.io>
2024-11-22 16:02:19 +0100

    Hyprland on Workstation

--- hosts/workstation/home.nix
+++ hosts/workstation/home.nix
@@ -1,22 +1,24 @@
-{ config, pkgs, ... }:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "noel";
   home.homeDirectory = "/home/noel";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  imports = [
+    (import ./../../modules/home-manager/vscode.nix { inherit pkgs; })
+    (import ./../../modules/home-manager/git.nix)
+    (import ./../../modules/home-manager/firefox.nix { inherit inputs pkgs; })
+  ];
+
+  home.stateVersion = "24.05";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
+
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
@@ -72,105 +74,6 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  programs.git = {
-    enable = true;
-    userName = "NoelHuibers";
-    userEmail = "noel@huibers.io";
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
-
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      esbenp.prettier-vscode
-      astro-build.astro-vscode
-      rust-lang.rust-analyzer
-      ms-python.black-formatter
-      github.copilot
-    ];
-    userSettings = {
-      "editor.inlineSuggest.enabled" = true;
-      "github.copilot.enable" = {
-        "*" = true;
-        "plaintext" = false;
-        "markdown" = false;
-        "scminput" = false;
-        "yaml" = false;
-      };
-      "workbench.colorTheme" = "Monokai";
-      "workbench.iconTheme" = "eq-material-theme-icons-palenight";
-      "git.autofetch" = true;
-      "editor.minimap.enabled" = false;
-      "terminal.integrated.fontFamily" = "\"JetBrainsMono NF\"";
-      "editor.formatOnSave" = true;
-      "workbench.sideBar.location" = "right";
-      "editor.tabSize" = 2;
-      "flake8.args" = [
-        "--max-line-length=88"
-        "ignore = E501, E402"
-        "extend-ignore = E203, W503"
-      ];
-      "python.analysis.autoImportCompletions" = true;
-      "workbench.settings.applyToAllProfiles" = [
-        "ltex.language"
-      ];
-      "ltex.language" = "en-US";
-      "ltex.additionalRules.enablePickyRules" = true;
-      "ltex.additionalRules.motherTongue" = "de-DE";
-      "tailwindCSS.includeLanguages.rs" = "html";
-      "cmake.configureOnOpen" = true;
-      "git.confirmSync" = false;
-
-      # Language-specific formatter settings
-      "[typescript]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[json]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[latex]" = {
-        "editor.defaultFormatter" = "mathematic.vscode-latex";
-      };
-      "[typescriptreact]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[javascript]" = {
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
-      };
-      "[astro]" = {
-        "editor.defaultFormatter" = "astro-build.astro-vscode";
-      };
-      "[rust]" = {
-        "editor.defaultFormatter" = "rust-lang.rust-analyzer";
-      };
-      "[python]" = {
-        "editor.defaultFormatter" = "ms-python.black-formatter";
-        "editor.codeActionsOnSave" = {
-          "source.organizeImports" = "explicit";
-        };
-        "editor.tabSize" = 4;
-      };
-      "[nix]" = {
-        "editor.defaultFormatter" = "jnoortheen.nix-ide";
-        "editor.tabSize" = 2;
-      };
-      "nix.serverPath" = "nixd";
-      "nix.enableLanguageServer" = true;
-      "nixpkgs.expr" = "import <nixpkgs> { }";
-      "nix.serverSettings" = {
-        "nixd" = {
-          "formatting" = {
-            "command" = [ "nixfmt" ];
-          };
-        };
-      };
-    };
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
